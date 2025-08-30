2914383cbc247724becf6e86f87cbd7274c602f4
StanislavJS <stasyk55@gmail.com>
2025-08-30 13:45:57 +0300

    fix [id]/page.tsx

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -1,3 +1,4 @@
+// app/(private routes)/notes/[id]/page.tsx
 import NotePreview from '../../../@modal/(.)notes/[id]/NotePreview.client';
 import { fetchNoteById } from '@/lib/api/clientApi';
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
@@ -6,18 +7,19 @@ import type { Metadata } from 'next';
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://your-vercel-domain.vercel.app';
 
 interface NotePageProps {
-  params: { id: string }; // id завжди має бути рядком
+  params: Promise<{ id: string }>;
 }
 
-// Хелпер для отримання нотатки
+// Хелпер для получения заметки
 async function getNoteById(id: string) {
   const note = await fetchNoteById(id);
   return note;
 }
 
-// Динамічні метадані сторінки
+// Динамические метаданные
 export async function generateMetadata({ params }: NotePageProps): Promise<Metadata> {
-  const note = await getNoteById(params.id);
+  const { id } = await params;
+  const note = await getNoteById(id);
 
   return {
     title: `${note.title} | NoteHub`,
@@ -25,7 +27,7 @@ export async function generateMetadata({ params }: NotePageProps): Promise<Metad
     openGraph: {
       title: `${note.title} | NoteHub`,
       description: note.content?.slice(0, 150) || 'Preview note in NoteHub.',
-      url: `${SITE_URL}/notes/${params.id}`,
+      url: `${SITE_URL}/notes/${id}`,
       images: [
         {
           url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
@@ -38,16 +40,17 @@ export async function generateMetadata({ params }: NotePageProps): Promise<Metad
   };
 }
 
-// Server component сторінки нотатки
+// Server Component страницы заметки
 export default async function NotePreviewPage({ params }: NotePageProps) {
-  const note = await getNoteById(params.id);
+  const { id } = await params;
+  const note = await getNoteById(id);
 
   const queryClient = new QueryClient();
-  queryClient.setQueryData(['note', params.id], note);
+  queryClient.setQueryData(['note', id], note);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotePreview noteId={params.id} />
+      <NotePreview noteId={id} />
     </HydrationBoundary>
   );
 }
