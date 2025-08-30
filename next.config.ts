369dcf964de2ae820ca517ecba1e2e5e43cfe884
StanislavJS <stasyk55@gmail.com>
2025-08-30 16:53:03 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['ac.goit.global', 'example.com'], // додай потрібні домени
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
+
